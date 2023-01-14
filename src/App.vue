cbc4fda4ab11cffad65750f52d7543627a340e82
Ibimina <ibimina.c.hart@gmail.com>
2023-01-14 14:58:00 +0100

    docs: refactored code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <script setup>
+import { ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+const steps = ref([
+  { no: 1, step: "STEP 1", name: "Your Info", to: "/" },
+  { no: 2, step: "STEP 2", name: "Select Plan", to: "/plan" },
+  { no: 3, step: "STEP 3", name: "Add-ons", to: "/add-ons" },
+  { no: 4, step: "STEP 4", name: "Summary", to: "/summary" },
+]);
 </script>
 
 <template>
   <header>
     <nav>
-      <RouterLink to="/" class="navlink">1</RouterLink>
-      <RouterLink to="/plan" class="navlink">2</RouterLink>
-      <RouterLink to="/add-ons" class="navlink">3</RouterLink>
-      <RouterLink to="/summary" class="navlink">4</RouterLink>
+      <RouterLink
+        v-for="step in steps"
+        :key="step.step"
+        :to="`/${step.to}`"
+        class="navlink"
+      >
+        <span class="step_no">{{ step.no }}</span>
+        <div class="step_title">
+          <span class="step">{{ step.step }}</span>
+          <span class="step_name">{{ step.name }}</span>
+        </div>
+      </RouterLink>
     </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  background-image: url("./assets/images/bg-sidebar-mobile.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: 10em;
+}
+.step_title {
+  display: none;
+}
+@media (min-width: 60rem) {
+  header {
+    background-image: url("./assets/images/bg-sidebar-desktop.svg");
+  }
+  .step_title {
+    display: block;
+  }
+}
+</style>
