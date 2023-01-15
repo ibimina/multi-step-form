2f62f0e8288d6087a1d8d91ca93c0e050b241bfd
Ibimina <ibimina.c.hart@gmail.com>
2023-01-15 18:34:05 +0100

    docs: persist user personal information

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,12 +17,22 @@ const getPersonalInfo = () => {
   const phone = document.querySelector("#phonenumber").value;
   personalInfo.value = { name, email, phone };
 };
+let storage = JSON.parse(localStorage.getItem("personalInfo"));
+
+if (storage) {
+  personalInfo.value = {
+    name: storage.name,
+    email: storage.email,
+    phone: storage.phone,
+  };
+}
 const handleNextStep = () => {
   if (
     personalInfo.value.name &&
     personalInfo.value.email &&
     personalInfo.value.phone
   ) {
+    localStorage.setItem("personalInfo", JSON.stringify(personalInfo.value));
     route.push("/plan");
   }
 };
@@ -31,45 +41,54 @@ const handleNextStep = () => {
 <template>
   <SelectButton />
 
-  <RegForm
-    title="Personal info"
-    subtitle="Please provide your name, email address, and phone number"
-  >
-    <div>
+  <RegForm>
+    <template v-slot:header>
+      <h3 class="title">Personal info</h3>
+      <p class="subtitle">
+        Please provide your name, email address, and phone number
+      </p>
+    </template>
+    <template v-slot:body
+      ><div>
+        <div>
+          <label for="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="e.g Stephen King"
+            required
+            :value="personalInfo.name"
+            @input="getPersonalInfo"
+          />
+        </div>
+        <div>
+          <label for="email">Email Address</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="stepehenking@lorem.com"
+            required
+            :value="personalInfo.email"
+            @input="getPersonalInfo"
+          />
+        </div>
+        <div>
+          <label for="phonenumber">Phone Number</label>
+          <input
+            type="tel"
+            id="phonenumber"
+            placeholder="e.g. +1 234 567 890"
+            required
+            :value="personalInfo.phone"
+            @input="getPersonalInfo"
+          />
+        </div></div
+    ></template>
+
+    <template v-slot:footer>
       <div>
-        <label for="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="e.g Stephen King"
-          required
-          :value="personalInfo.name"
-          @input="getPersonalInfo"
-        />
+        <button @click="handleNextStep">Next Step</button>
       </div>
-      <div>
-        <label for="email">Email Address</label>
-        <input
-          type="text"
-          id="email"
-          placeholder="stepehenking@lorem.com"
-          required
-          @input="getPersonalInfo"
-        />
-      </div>
-      <div>
-        <label for="phonenumber">Phone Number</label>
-        <input
-          type="tel"
-          id="phonenumber"
-          placeholder="e.g. +1 234 567 890"
-          required
-          @input="getPersonalInfo"
-        />
-      </div>
-    </div>
-    <div>
-      <button @click="handleNextStep">Next Step</button>
-    </div>
+    </template>
   </RegForm>
 </template>
