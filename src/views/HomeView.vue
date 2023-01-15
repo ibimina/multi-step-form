420617a6db575f30bb0ccbc33fc68f202c55ebda
Ibimina <ibimina.c.hart@gmail.com>
2023-01-15 01:25:08 +0100

    docs: personal info form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,75 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import RegForm from "../components/RegForm.vue";
+import SelectButton from "../components/SelectButton.vue";
+
+const route = useRouter();
+const personalInfo = ref({
+  name: "",
+  email: "",
+  phone: "",
+});
+
+const getPersonalInfo = () => {
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const phone = document.querySelector("#phonenumber").value;
+  personalInfo.value = { name, email, phone };
+};
+const handleNextStep = () => {
+  if (
+    personalInfo.value.name &&
+    personalInfo.value.email &&
+    personalInfo.value.phone
+  ) {
+    route.push("/plan");
+  }
+};
+</script>
 
 <template>
-  <main>
-    <p>home</p>
-    <!-- Step 1 Your info Step 2 Select plan Step 3 Add-ons Step 4 Summary Personal info Please provide your name, email address,
-    and phone number. Name e.g. Stephen King Email Address e.g. stephenking@lorem.com Phone Number e.g. +1 234 567 890 Next
-    Step Select your plan You have the option of monthly or yearly billing. Arcade $9/mo Advanced $12/mo Pro $15/mo Monthly
-    Yearly Go Back Next Step Pick add-ons Add-ons help enhance your gaming experience. Online service Access to multiplayer
-    games +$1/mo Larger storage Extra 1TB of cloud save +$2/mo Customizable Profile Custom theme on your profile +$2/mo Go
-    Back Next Step Finishing up Double-check everything looks OK before confirming. Total (per month/year) Go Back Confirm
-    Thank you! Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support,
-    please feel free to email us at support@loremgaming.com.
-    Challenge by Frontend Mentor. Coded by Your Name Here. -->
-  </main>
+  <SelectButton />
+
+  <RegForm
+    title="Personal info"
+    subtitle="Please provide your name, email address, and phone number"
+  >
+    <div>
+      <div>
+        <label for="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="e.g Stephen King"
+          required
+          :value="personalInfo.name"
+          @input="getPersonalInfo"
+        />
+      </div>
+      <div>
+        <label for="email">Email Address</label>
+        <input
+          type="text"
+          id="email"
+          placeholder="stepehenking@lorem.com"
+          required
+          @input="getPersonalInfo"
+        />
+      </div>
+      <div>
+        <label for="phonenumber">Phone Number</label>
+        <input
+          type="tel"
+          id="phonenumber"
+          placeholder="e.g. +1 234 567 890"
+          required
+          @input="getPersonalInfo"
+        />
+      </div>
+    </div>
+    <div>
+      <button @click="handleNextStep">Next Step</button>
+    </div>
+  </RegForm>
 </template>
