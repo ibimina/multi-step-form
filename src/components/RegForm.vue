a1f160117fd5a68736327df1dfaaf3abd87bc7d4
Ibimina <ibimina.c.hart@gmail.com>
2023-01-15 01:23:47 +0100

    docs: form resuseable component

---
+++ src/components/RegForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h3 class="title"></h3>
+    <p class="subtitle"></p>
+    <slot></slot>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  padding: 2em 1em;
+}
+</style>
