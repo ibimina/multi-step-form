4eecb4f2d72ae8e39e35233322fc8b9d7f176fb9
Ibimina <ibimina.c.hart@gmail.com>
2023-01-15 16:23:39 +0100

    docs: form summary template

--- src/views/SummaryView.vue
+++ src/views/SummaryView.vue
@@ -1,5 +1,32 @@
-<script setup></script>
+<script setup>
+import RegForm from "../components/RegForm.vue";
+import SelectButton from "../components/SelectButton.vue";
+import path from "../path";
+path();
+</script>
 
 <template>
-  <p>summary</p>
+  <SelectButton />
+  <RegForm
+    title="Finishing up"
+    subtitle="Double check everything looks OK before confirming."
+  >
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>Arcade(Monthly)</p>
+            <p>Change</p>
+          </div>
+          <p>$9/mo</p>
+        </div>
+        <div>
+          <p>online service <span>+$2/mo</span></p>
+          <p>Larger storage <span>+$2/mo</span></p>
+        </div>
+      </div>
+      <p>Total () <span></span></p>
+    </div>
+    <button>confirm</button>
+  </RegForm>
 </template>
