fdb784512f076db87b933abe1cdca68acc47f7ca
Ibimina <ibimina.c.hart@gmail.com>
2023-01-15 18:44:22 +0100

    docs: reusable component

---
+++ src/components/GoBack.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useRouter } from "vue-router";
+const route = useRouter();
+const handleGoBack = () => {
+  route.back();
+};
+</script>
+
+<template>
+  <button @click="handleGoBack">GoBck</button>
+</template>
