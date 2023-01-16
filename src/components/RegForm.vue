ab5d751815644369cf9310c2ddded80f84b8fa5f
Ibimina <ibimina.c.hart@gmail.com>
2023-01-16 16:31:21 +0100

    styles: styled the main container

--- src/components/RegForm.vue
+++ src/components/RegForm.vue
@@ -8,7 +8,13 @@
 
 <style scoped>
 main {
+  max-width: 350px;
   width: 100%;
   padding: 2em 1em;
+  border-radius: 8px;
+  position: relative;
+  top: -5.2em;
+  margin: auto;
+  background-color: hsl(0, 0%, 100%);
 }
 </style>
