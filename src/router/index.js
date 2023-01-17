2cc09a10abf9bcb93e26544c451b0fe58c1f14d1
Ibimina <ibimina.c.hart@gmail.com>
2023-01-17 16:23:01 +0100

    docs: change route path

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const router = createRouter({
       component: () => import("../views/SelectPlan.vue"),
     },
     {
-      path: "/add-ons",
+      path: "/addons",
       name: "Addons",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
