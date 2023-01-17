d7267c429437b5602686cd8a065f9db869568237
Ibimina <ibimina.c.hart@gmail.com>
2023-01-17 17:54:01 +0100

    docs: correct path name

--- src/path.js
+++ src/path.js
@@ -3,7 +3,7 @@ export default function path() {
   const steps = ref([
     { no: 1, step: "STEP 1", name: "Your Info", to: "/", isPath: true },
     { no: 2, step: "STEP 2", name: "Select Plan", to: "/plan", isPath: false },
-    { no: 3, step: "STEP 3", name: "Add-ons", to: "/add-ons", isPath: false },
+    { no: 3, step: "STEP 3", name: "Add-ons", to: "/addons", isPath: false },
     { no: 4, step: "STEP 4", name: "Summary", to: "/summary", isPath: false },
   ]);
   const currentRoute = window.location.pathname;
