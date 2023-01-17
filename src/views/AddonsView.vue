bbd914b40ef49e91a3780d962bf37404d750b68a
Ibimina <ibimina.c.hart@gmail.com>
2023-01-17 03:56:47 +0100

    docs: refactored code

--- src/views/AddonsView.vue
+++ src/views/AddonsView.vue
@@ -54,17 +54,14 @@ const getAddOns = (e, val) => {
       ...addOnsValue.value,
       { name: val.addon, amount: val.amount, checked: e.target.checked },
     ];
-    addOns.value = addOns.value.map((on) =>
-      on.name === val.addon ? { ...on, checked: e.target.checked } : on
-    );
   } else {
     addOnsValue.value = addOnsValue.value.filter(
       (addon) => addon.name !== val.addon
     );
-    addOns.value = addOns.value.map((on) =>
-      on.name === val.addon ? { ...on, checked: e.target.checked } : on
-    );
   }
+  addOns.value = addOns.value.map((on) =>
+    on.name === val.name ? { ...on, checked: e.target.checked } : on
+  );
   localStorage.setItem("addons", JSON.stringify(addOnsValue.value));
 };
 
@@ -80,30 +77,152 @@ const handleNextStep = () => {
   <RegForm>
     <template v-slot:header>
       <h3 class="title">Pick add-ons</h3>
-      <p class="subtitle">You have the option of monthly or yearly billing</p>
-      <p>Add-ons help enhance your gaming experience</p>
+      <p class="subtitle">Add-ons help enhance your gaming experience</p>
     </template>
     <template v-slot:body>
       <div>
-        <label v-for="addon in addOns" :key="addon.name">
-          <input
-            type="checkbox"
-            name=""
-            id=""
-            @change="getAddOns($event, addon)"
-            :checked="addon.checked"
-          />
-          <div>
-            <span>{{ addon.addon }}</span> <span>{{ addon.desc }}</span>
+        <label
+          v-for="addon in addOns"
+          :key="addon.name"
+          class="pick"
+          :class="{ check: addon.checked }"
+        >
+          <div class="pick_col">
+            <div class="checkbox_con">
+              <input
+                class="checkbox"
+                type="checkbox"
+                @change="getAddOns($event, addon)"
+                :checked="addon.checked"
+              />
+              <span class="checkmark"></span>
+            </div>
+            <div class="pick_desc">
+              <span class="add_name">{{ addon.addon }}</span>
+              <span class="pic_des">{{ addon.desc }}</span>
+            </div>
           </div>
-          <p v-if="paymentType.type">+${{ addon.amount }}/yr</p>
-          <p v-else>+${{ addon.amount }}/mo</p>
+          <p v-if="paymentType.type" class="amt">+${{ addon.amount }}/yr</p>
+          <p v-else class="amt">+${{ addon.amount }}/mo</p>
         </label>
       </div>
     </template>
     <template v-slot:footer>
       <GoBack />
-      <button @click="handleNextStep">Next Step</button>
+      <button @click="handleNextStep" class="left">Next Step</button>
     </template>
   </RegForm>
 </template>
+
+<style>
+.pick {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5em;
+  border-radius: 7px;
+  border: 1px solid hsl(229, 24%, 87%);
+  margin-bottom: 1em;
+  box-shadow: 0px 3px 0px 0px rgba(221, 221, 221, 0.5);
+  cursor: pointer;
+}
+.pick:hover {
+  border: 1px solid hsl(243, 100%, 62%);
+  box-shadow: none;
+}
+.add_name {
+  color: hsl(213, 96%, 18%);
+  font-weight: 500;
+}
+.pick_col {
+  display: flex;
+  align-items: center;
+  gap: 1em;
+}
+.pick_desc {
+  display: flex;
+  flex-direction: column;
+}
+.check {
+  border: 1px solid hsl(243, 100%, 62%);
+  background-color: hsl(217, 100%, 97%);
+  box-shadow: none;
+}
+
+.pic_des {
+  font-size: 14px;
+  color: hsl(229, 24%, 87%);
+}
+.checkbox_con {
+  display: block;
+  position: relative;
+  padding-left: 20px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.checkbox_con input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+.amt {
+  color: hsl(243, 100%, 62%);
+  font-size: 12px;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 20px;
+  width: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  border: 1px solid hsl(229, 24%, 87%);
+}
+
+/* On mouse-over, add a grey background color */
+/* .container:hover input ~ .checkmark {
+  background-color: #ccc;
+} */
+
+/* When the checkbox is checked, add a blue background */
+.checkbox_con input:checked ~ .checkmark {
+  background-color: hsl(243, 100%, 62%);
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.checkbox_con input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.checkbox_con .checkmark:after {
+  left: 7px;
+  top: 3px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+</style>
