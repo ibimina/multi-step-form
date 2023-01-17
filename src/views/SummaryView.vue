058e3fe79a8beafa64775b84428d9ef99aa88ad9
Ibimina <ibimina.c.hart@gmail.com>
2023-01-17 03:59:10 +0100

    docs: refactored code, added styles to component

--- src/views/SummaryView.vue
+++ src/views/SummaryView.vue
@@ -17,15 +17,25 @@ const handleConfirm = () => {
 
 <template>
   <SelectButton />
-  <div v-if="confirm">
-    <img src="/images/icon-thank-you.svg" alt="successful checkmark" />
-    <h3>Thank you</h3>
-    <p>Thanks for confirming your subscription!</p>
-    <p>
-      We hope you have fun using our platform. if you ever need support, please
-      feel free to email us at support@loremgaming.com
-    </p>
-  </div>
+  <RegForm v-if="confirm">
+    <template v-slot:thankyou>
+      <div class="thanks">
+        <img
+          src="/images/icon-thank-you.svg"
+          alt="successful checkmark"
+          class="check_icon"
+        />
+        <h3 class="thanks_text">Thank you</h3>
+        <p class="summary_addon nomargin">
+          Thanks for confirming your subscription!
+        </p>
+        <p class="summary_addon nomargin">
+          We hope you have fun using our platform. if you ever need support,
+          please feel free to email us at support@loremgaming.com
+        </p>
+      </div>
+    </template>
+  </RegForm>
   <RegForm v-else>
     <template v-slot:header>
       <h3 class="title">Finishing up</h3>
@@ -33,37 +43,110 @@ const handleConfirm = () => {
         Double-check everything looks OK before confirming.
       </p>
     </template>
-    <template v-slot:body
-      ><div>
-        <div>
-          <div>
+    <template v-slot:body>
+      <div>
+        <div class="summary">
+          <div class="summary_flex summary_plan">
             <div>
-              <p>{{ plan.name }} (Monthly)</p>
-              <p>Change</p>
+              <p class="adds bold">{{ plan.name }} (Monthly)</p>
+              <RouterLink to="/plan" class="summary_addon link"
+                >Change</RouterLink
+              >
             </div>
-            <p v-if="plan.type">${{ plan.amount }}/yr</p>
-            <p v-else>${{ plan.amount }}/mo</p>
+            <p v-if="plan.type" class="adds bold">${{ plan.amount }}/yr</p>
+            <p v-else class="adds bold">${{ plan.amount }}/mo</p>
           </div>
           <div>
-            <p v-for="add in addon" :key="add.name">
+            <p
+              v-for="add in addon"
+              :key="add.name"
+              class="summary_addon summary_flex"
+            >
               <span>{{ add.name }}</span>
-              <span v-if="plan.type"> +${{ add.amount }}/yr</span>
-              <span v-else> +${{ add.amount }}/mo</span>
+              <span v-if="plan.type" class="adds"> +${{ add.amount }}/yr</span>
+              <span v-else class="adds"> +${{ add.amount }}/mo</span>
             </p>
           </div>
         </div>
-        <p v-if="plan.type">
-          Total (per year) <span>+${{ total }}/yr</span>
+        <p v-if="plan.type" class="total summary_flex">
+          Total (per year) <span class="totalvalue">+${{ total }}/yr</span>
         </p>
-        <p v-else>
-          Total (per month) <span>+${{ total }}</span
-          >/mo
+        <p v-else class="total summary_flex">
+          Total (per month) <span class="totalvalue">+${{ total }}/mo</span>
         </p>
       </div>
     </template>
     <template v-slot:footer>
       <GoBack />
-      <button @click="handleConfirm">Confirm</button>
+      <button @click="handleConfirm" class="left blue">Confirm</button>
     </template>
   </RegForm>
 </template>
+<style>
+.summary {
+  background-color: hsl(231, 100%, 99%);
+  padding: 1em;
+  border-radius: 8px;
+  margin-bottom: 1em;
+}
+.summary_flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.summary_plan {
+  padding-bottom: 0.5em;
+  margin-bottom: 0.5em;
+  border-bottom: 1px solid rgb(226, 225, 225);
+}
+.totalvalue {
+  color: blue;
+  font-weight: 700;
+}
+.adds {
+  color: hsl(213, 96%, 18%);
+  font-weight: 500;
+}
+.link {
+  text-decoration: underline;
+}
+.bold {
+  font-weight: 700;
+}
+.blue {
+  background-color: blue;
+}
+.summary_addon {
+  color: hsl(231, 11%, 63%);
+  margin-bottom: 0.5em;
+}
+.total {
+  padding: 0 1em;
+  color: hsl(231, 11%, 63%);
+}
+.thanks {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 3em auto;
+}
+.check_icon {
+  margin-bottom: 1.5em;
+}
+.thanks_text {
+  margin-bottom: 0.5em;
+}
+.nomargin {
+  margin-bottom: 0;
+  padding: 0 0.5em;
+  text-align: center;
+}
+.link:hover {
+  color: blue;
+}
+.blue:hover {
+  opacity: 0.5;
+  cursor: pointer;
+}
+</style>
